891dd7e3f19a5cd663d9755aa17ece6ff2aded01
Randi Stella <RANDINICOLESTELLA@GMAIL.COM>
2023-03-10 10:05:12 -0600

    proj 8 done

---
+++ 8-form-wave-animation/app.js
@@ -0,0 +1,9 @@
+const labels = document.querySelectorAll('.form-control label');
+
+// wraps each letter in a span
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('');
+});
